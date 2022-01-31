5459013bdc83062bec7c60f72630ce2ff726fe6b
Cristián Ormazábal (OrmaSoft) <cristian@ormasoft.cl>
2022-01-31 00:48:29 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const purgecss = require('gulp-purgecss');
+
+gulp.task('styles', () => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);
+});
+
+gulp.task('purgecss', () => {
+  return gulp.src('css/**/*.css')
+      .pipe(purgecss({
+          content: ['../clubster-app/src/**/*.html']
+      }))
+      .pipe(gulp.dest('output'))
+});
+
+gulp.task('watch', () => {
+    gulp.watch('sass/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles', 'purgecss'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['clean', 'styles', 'purgecss']));
+
+
